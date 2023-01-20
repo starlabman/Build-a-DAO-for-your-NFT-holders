98ce50ecd1d359188ab17b019f4c7ab4de5c46e7
labore <akodjolabore@gmail.com>
2023-01-20 11:42:01 +0000

    DAO contract add

--- hardhat-tutorial/contracts/CryptoDevsDAO.sol
+++ hardhat-tutorial/contracts/CryptoDevsDAO.sol
@@ -84,3 +84,115 @@ modifier nftHolderOnly() {
     require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
     _;
 }
+
+/// @dev createProposal allows a CryptoDevsNFT holder to create a new proposal in the DAO
+/// @param _nftTokenId - the tokenID of the NFT to be purchased from FakeNFTMarketplace if this proposal passes
+/// @return Returns the proposal index for the newly created proposal
+function createProposal(uint256 _nftTokenId)
+    external
+    nftHolderOnly
+    returns (uint256)
+{
+    require(nftMarketplace.available(_nftTokenId), "NFT_NOT_FOR_SALE");
+    Proposal storage proposal = proposals[numProposals];
+    proposal.nftTokenId = _nftTokenId;
+    // Set the proposal's voting deadline to be (current time + 5 minutes)
+    proposal.deadline = block.timestamp + 5 minutes;
+
+    numProposals++;
+
+    return numProposals - 1;
+}
+
+// Create a modifier which only allows a function to be
+// called if the given proposal's deadline has not been exceeded yet
+modifier activeProposalOnly(uint256 proposalIndex) {
+    require(
+        proposals[proposalIndex].deadline > block.timestamp,
+        "DEADLINE_EXCEEDED"
+    );
+    _;
+}
+
+// Create an enum named Vote containing possible options for a vote
+enum Vote {
+    YAY, // YAY = 0
+    NAY // NAY = 1
+}
+
+/// @dev voteOnProposal allows a CryptoDevsNFT holder to cast their vote on an active proposal
+/// @param proposalIndex - the index of the proposal to vote on in the proposals array
+/// @param vote - the type of vote they want to cast
+function voteOnProposal(uint256 proposalIndex, Vote vote)
+    external
+    nftHolderOnly
+    activeProposalOnly(proposalIndex)
+{
+    Proposal storage proposal = proposals[proposalIndex];
+
+    uint256 voterNFTBalance = cryptoDevsNFT.balanceOf(msg.sender);
+    uint256 numVotes = 0;
+
+    // Calculate how many NFTs are owned by the voter
+    // that haven't already been used for voting on this proposal
+    for (uint256 i = 0; i < voterNFTBalance; i++) {
+        uint256 tokenId = cryptoDevsNFT.tokenOfOwnerByIndex(msg.sender, i);
+        if (proposal.voters[tokenId] == false) {
+            numVotes++;
+            proposal.voters[tokenId] = true;
+        }
+    }
+    require(numVotes > 0, "ALREADY_VOTED");
+
+    if (vote == Vote.YAY) {
+        proposal.yayVotes += numVotes;
+    } else {
+        proposal.nayVotes += numVotes;
+    }
+}
+
+// Create a modifier which only allows a function to be
+// called if the given proposals' deadline HAS been exceeded
+// and if the proposal has not yet been executed
+modifier inactiveProposalOnly(uint256 proposalIndex) {
+    require(
+        proposals[proposalIndex].deadline <= block.timestamp,
+        "DEADLINE_NOT_EXCEEDED"
+    );
+    require(
+        proposals[proposalIndex].executed == false,
+        "PROPOSAL_ALREADY_EXECUTED"
+    );
+    _;
+}
+
+/// @dev executeProposal allows any CryptoDevsNFT holder to execute a proposal after it's deadline has been exceeded
+/// @param proposalIndex - the index of the proposal to execute in the proposals array
+function executeProposal(uint256 proposalIndex)
+    external
+    nftHolderOnly
+    inactiveProposalOnly(proposalIndex)
+{
+    Proposal storage proposal = proposals[proposalIndex];
+
+    // If the proposal has more YAY votes than NAY votes
+    // purchase the NFT from the FakeNFTMarketplace
+    if (proposal.yayVotes > proposal.nayVotes) {
+        uint256 nftPrice = nftMarketplace.getPrice();
+        require(address(this).balance >= nftPrice, "NOT_ENOUGH_FUNDS");
+        nftMarketplace.purchase{value: nftPrice}(proposal.nftTokenId);
+    }
+    proposal.executed = true;
+}
+/// @dev withdrawEther allows the contract owner (deployer) to withdraw the ETH from the contract
+function withdrawEther() external onlyOwner {
+    uint256 amount = address(this).balance;
+    require(amount > 0, "Nothing to withdraw; contract balance empty");
+    payable(owner()).transfer(amount);
+}
+
+// The following two functions allow the contract to accept ETH deposits
+// directly from a wallet without calling a function
+receive() external payable {}
+
+fallback() external payable {}
