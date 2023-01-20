3253c551bff119ed19796b1d119569f1382bfc52
labore <akodjolabore@gmail.com>
2023-01-20 15:57:52 +0000

    update the Dao contract

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -5,7 +5,7 @@ const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
     goerli: {
       url: QUICKNODE_HTTP_URL,
